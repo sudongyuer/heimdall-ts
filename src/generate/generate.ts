8915c146adf325cd31bfb779aa25ab212efa6689
sudongyu <976499226@qq.com>
2021-10-28 21:20:53 +0800

    1.1.27

--- src/generate/generate.ts
+++ src/generate/generate.ts
@@ -11,38 +11,41 @@ const {generateApi} = require('swagger-typescript-api');
 /**
  * 创建api文件
  */
-async function createApi() {
-    //V3
-    /* NOTE: all fields are optional expect one of `output`, `url`, `spec` */
-    const openApi3Array = getOenAPI3YmlFileName(path.resolve(cwd(), `${getProjectName()}`))
-    for (let item of openApi3Array) {
-        await generateApi({
-            name: `${item.replace('.oas3.yml', '')}Api.ts`,
-            url: null,
-            output: path.resolve(process.cwd(), "node_modules/@imf/heimdall-ts/api"),
-            input: path.resolve(process.cwd(), `${getProjectName()}`, `${item}`),
-            httpClientType: "axios", // or "fetch",
-            unwrapResponseData:true,
-            generateUnionEnums:true,
-            enumNamesAsValues:true
-        })
-    }
+ function createApi() {
+     return new Promise<void>((resolve, reject)=>{
+         //V3
+         /* NOTE: all fields are optional expect one of `output`, `url`, `spec` */
+         const openApi3Array = getOenAPI3YmlFileName(path.resolve(cwd(), `${getProjectName()}`))
+         for (let item of openApi3Array) {
+             generateApi({
+                 name: `${item.replace('.oas3.yml', '')}Api.ts`,
+                 url: null,
+                 output: path.resolve(process.cwd(), "node_modules/@imf/heimdall-ts/api"),
+                 input: path.resolve(process.cwd(), `${getProjectName()}`, `${item}`),
+                 httpClientType: "axios", // or "fetch",
+                 unwrapResponseData:true,
+                 generateUnionEnums:true,
+                 enumNamesAsValues:true
+             })
+         }
 
-    //V2
-    /* NOTE: all fields are optional expect one of `output`, `url`, `spec` */
-    const openApi2Array = getOenAPI2YmlFileName(path.resolve(cwd(), `${getProjectName()}`))
-    for (let item of openApi2Array) {
-        await generateApi({
-            name: `${item.replace('.oas2.yml', '')}Api.ts`,
-            url: null,
-            output: path.resolve(process.cwd(), "node_modules/@imf/heimdall-ts/api"),
-            input: path.resolve(process.cwd(), `${getProjectName()}`, `${item}`),
-            httpClientType: "axios", // or "fetch",
-            unwrapResponseData:true,
-            generateUnionEnums:true,
-            enumNamesAsValues:true
-        })
-    }
+         //V2
+         /* NOTE: all fields are optional expect one of `output`, `url`, `spec` */
+         const openApi2Array = getOenAPI2YmlFileName(path.resolve(cwd(), `${getProjectName()}`))
+         for (let item of openApi2Array) {
+             generateApi({
+                 name: `${item.replace('.oas2.yml', '')}Api.ts`,
+                 url: null,
+                 output: path.resolve(process.cwd(), "node_modules/@imf/heimdall-ts/api"),
+                 input: path.resolve(process.cwd(), `${getProjectName()}`, `${item}`),
+                 httpClientType: "axios", // or "fetch",
+                 unwrapResponseData:true,
+                 generateUnionEnums:true,
+                 enumNamesAsValues:true
+             })
+         }
+        resolve()
+     })
 
 
 }
