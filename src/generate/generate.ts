5684d68e4b56dce51784b062e5ba8d33278fc536
sudongyu <976499226@qq.com>
2022-02-23 18:17:01 +0800

    chore: add asgardApi function

--- src/generate/generate.ts
+++ src/generate/generate.ts
@@ -1,15 +1,26 @@
 import {createRequire} from "module";
-import {getOenAPI2YmlFileName, getOenAPI3YmlFileName, getOpenAPIFileName, removeDir} from '../utils/file/index.js'
+import {
+    getOenAPI2YmlFileName,
+    getOenAPI3YmlFileName,
+    getOpenAPIFileName,
+    patchStringToFile,
+    removeDir
+} from '../utils/file/index.js'
 import {cwd} from "process";
 import {getProjectName} from "../utils/common/index.js";
 const require = createRequire(import.meta.url);
 const path = require("path");
 const {generateApi} = require('swagger-typescript-api');
+import * as fs from "fs";
+import openapiTS from "openapi-typescript";
+import * as fes from 'fs-extra'
+import {asgardTemplateCodeGen} from "../template/asgardTemplate.js";
+
 
 /**
  * 创建api文件
  */
-function createApi(repo) {
+function createHTTPApi(repo) {
     return new Promise<void>(async (resolve, reject) => {
         //V3
         /* NOTE: all fields are optional expect one of `output`, `url`, `spec` */
@@ -38,13 +49,87 @@ function createApi(repo) {
 }
 
 /**
- * 基于多个仓库生成API
+ * 创建api文件
  */
-function createMultiApi(repos){
+function createAsgardApi(repoName) {
+    return new Promise<void>(async (resolve, reject) => {
+        //V3
+        /* NOTE: all fields are optional expect one of `output`, `url`, `spec` */
+        const openApiFileNames = getOpenAPIFileName(path.resolve(cwd(), `${repoName}`))
+        console.log('=======',openApiFileNames)
+        for (let fileName of openApiFileNames) {
+            //生成类型
+            await generateApiTypes(repoName,fileName)
+            //生成具体的API
+            await generateApiCode(repoName,fileName)
+        }
+        if(!openApiFileNames.length){
+            await removeDir(path.resolve(cwd(), repoName))
+            reject('no openApi3 or openApi2 resources to generate !!!!!')
+        }
+        resolve()
+    })
+}
+
+/**
+ *
+ * @param repoName 仓库名称(规范文件夹)
+ * @param fileName 文件名称(规范文件)
+ */
+export  function generateApiCode(repoName, fileName) {
+    return new Promise<void>(async (resolve, reject) => {
+        //读取文件对象
+        //TODO 读取文件失败
+        const jsonObjString = await fs.promises.readFile(path.resolve(cwd(),repoName,fileName), "utf8")
+        const jsonObj=JSON.parse(jsonObjString)
+        //生成该文件所对应的代码
+        const generateCode = asgardTemplateCodeGen(jsonObj)
+        //将生成好的代码patch到指定的文件中
+        await patchStringToFile(path.resolve(cwd(), 'node_modules/@imf/heimdall-ts/api', fileName.replace(/(.json|.yaml|.yml)$/, '.ts')), generateCode)
+        resolve()
+    })
+
+}
+
+/**
+ * 创建api文件
+ */
+function generateApiTypes(repoName,fileName){
+    return new Promise<void>(async (resolve, reject) => {
+        //读取约束
+        const schema = await fs.promises.readFile(path.resolve(cwd(),repoName,fileName), "utf8") // must be OpenAPI JSON
+        //生成ts类型
+        const output = await openapiTS(JSON.parse(schema));
+        //将约束写进ts文件
+        await fes.outputFile(path.resolve(cwd(), 'node_modules/@imf/heimdall-ts/api', fileName.replace(/(.json|.yaml|.yml)$/,'.ts')), output)
+        resolve()
+    })
+}
+
+/**
+ * 基于多个仓库生成HTTP API
+ */
+function createMultiHTTPApi(repos){
     return new Promise<void>((resolve, reject)=>{
         const promiseArray=[]
-        for (const [repo,versionCode] of repos) {
-            promiseArray.push(createApi(repo))
+        for (const [repoName,repoObj] of repos) {
+            promiseArray.push(createHTTPApi(repoName))
+        }
+        Promise.all(promiseArray).then(()=>{
+            resolve()
+        })
+    })
+}
+
+/**
+ * 基于多个仓库生成Asgard API
+ */
+function createMultiAsgardApi(repos){
+    console.log(repos)
+    return new Promise<void>((resolve, reject)=>{
+        const promiseArray=[]
+        for (const [repoName,repoObj] of repos) {
+            promiseArray.push(createAsgardApi(repoName))
         }
         Promise.all(promiseArray).then(()=>{
             resolve()
@@ -53,6 +138,8 @@ function createMultiApi(repos){
 }
 
 export {
-    createApi,
-    createMultiApi
+    createHTTPApi,
+    createAsgardApi,
+    createMultiHTTPApi,
+    createMultiAsgardApi
 }
